f7a124a9f7ace402a3e97cc95089f18e297be107
Marshal <marshyordanos@gmail.com>
2024-09-09 15:25:31 +0300

    chnages

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,7 +114,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
 
     const token = jwt.sign({ userId: user._id }, "some_secrete", {
-      expiresIn: "1m",
+      expiresIn: "1d",
     });
     const resUser = filterObj(
       user["_doc"],
